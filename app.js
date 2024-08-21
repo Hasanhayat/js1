b575299af2014dc7fc2af21a02ea3249edffa5e8
Hasanhayat <aylskhsmvz.v@gmail.com>
2024-08-21 23:14:27 +0500

    f

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 console.log("ASSALAM O ALIKUM");
+//first
+let btn1 = document.querySelector("#btn1");
 
-let btn = document.querySelector("#btn");
+btn1.addEventListener("click", fun1);
 
-btn.addEventListener("click",fun1);
+function fun1() {
+  alert("Error! please enter valid password");
+  alert("Welcome to js land...\nHappy Coding!");
+}
+//second
+let btn2 = document.querySelector("#btn2");
 
+btn2.addEventListener("click", fun2);
 
-function fun1(){
-    
-
-
-
+function fun2() {
+  alert("Error! please enter valid password");
+  alert("Welcome to js land...\nHappy Coding!");
 }
