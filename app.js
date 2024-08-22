eef06223c84d09dcd243759a503ad9a1f988f914
Hasanhayat <aylskhsmvz.v@gmail.com>
2024-08-22 23:10:27 +0500

    f

--- app.js
+++ app.js
@@ -10,10 +10,31 @@ function fun1() {
 }
 //second
 let btn2 = document.querySelector("#btn2");
-
 btn2.addEventListener("click", fun2);
+var username = "HASSAN HAYAT";
+let message = "HELLO WORLD";
+var studentName = "Hassan Hayat";
+var age = 16;
+var classLevel = "10th Grade";
+var schoolName = " High School";
+var city = "Karachi";
+let pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+const email = "aylskhsmvz.v@gmail.com";
+const book = "“A smarter way to learn JavaScript”. ";
+var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+let p = document.querySelector("h1");
 
 function fun2() {
-  alert("Error! please enter valid password");
-  alert("Welcome to js land...\nHappy Coding!");
+  alert(username);
+  alert(message);
+  alert("Student Name: " + studentName);
+  alert("Age: " + age);
+  alert("Class Level: " + classLevel);
+  alert("School Name: " + schoolName);
+  alert("City: " + city);
+  alert(pizza);
+  alert("My Email is: " + email);
+  alert(book);
+  p.innerHTML = "Yah! I can write HTML through JavaScript";
+  alert(string);
 }
